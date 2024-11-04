e8bc706de7a953dd50de9367f2a6d6ef1f3625e8
Amina Usmanova <amina.usmanova564@gmail.com>
2024-11-04 23:32:49 +0500

    homeworks (some of them without css)

---
+++ script.js
@@ -0,0 +1,30 @@
+function calcTimer () {
+    let today = new Date ()
+    let saleDeadLine = new Date (2024, 10, 2)
+    let delta = (saleDeadLine - today)/1000
+    delta = Math.trunc(delta)
+
+    let hours = Math.trunc(delta / 3600)
+    let minutes = Math.trunc (delta / 60) - hours*60
+    let seconds = delta - hours*60*60 - minutes*60
+    let hoursElem = document.getElementById("timerHours")
+    let minutesElem = document.getElementById("timerMinutes")
+    let secondsElem = document.getElementById("timerSeconds")
+    hoursElem.innerHTML = twoDigits(hours)
+    minutesElem.innerHTML = twoDigits(minutes)
+    secondsElem.innerHTML = twoDigits (seconds)
+}
+
+    calcTimer ()
+    setInterval(calcTimer, 1000)
+
+    function twoDigits(num) {
+        if(num<10) {
+            return "0"+num
+        } else {
+            return num
+        }
+    }
+
+
+    
